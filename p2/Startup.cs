70752861db2128b266784e5b1067f12ea3a43afd
stu <stu@qq.com>
2016-10-19 09:36:55 +0800

    添加项目文件。

---
+++ p2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(p2.Startup))]
+namespace p2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
