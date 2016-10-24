3df660d4f2932158162b4bd96a4513b532e3f573
stu <stu@qq.com>
2016-10-24 16:02:22 +0800

    1017Hello页面迁入

--- p2/Controllers/HomeController.cs
+++ p2/Controllers/HomeController.cs
@@ -38,5 +38,17 @@ namespace p2.Controllers
             ViewData.Model = data;
             return View();
         }
+        public ActionResult Hello()
+        {
+            int a = 11;
+            int sum = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                sum += i;
+            }
+            ViewBag.sum = sum;
+
+            return View();
+        }
     }
 }
