0d1e836d5f7b9bab7ac4073e77637bd693fcd95d
stu <stu@qq.com>
2016-10-31 17:58:28 +0800

    博客数据库增加删除修改。

---
+++ p2/BlogDB.cs
@@ -0,0 +1,58 @@
+﻿namespace p2
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BlogDB : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“BlogDB”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“p2.BlogDB”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“BlogDB”
+        //连接字符串。
+        public BlogDB()
+            : base("name=BlogDB")
+        {
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+         public virtual DbSet<Blog> Blogs { get; set; }
+
+        public virtual DbSet<BlogArticle> BlogArticles { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var blogTable = modelBuilder.Entity<Blog>();
+            var blogArtticleTable = modelBuilder.Entity<BlogArticle>();
+            blogTable.HasKey(o => o.ID);
+            blogArtticleTable.HasKey(o => o.ID);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+    public class BlogArticle
+    {
+        public int ID { get; set; }
+        public string BlogID { get; set; }
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime CreTime { get; set; }
+    }
+        public class Blog
+        {
+            public int ID { get; set; }
+            public string Title { get; set; }
+        }
+
+
+        //public class MyEntity
+        //{
+        //    public int Id { get; set; }
+        //    public string Name { get; set; }
+        //}
+    }
---
+++ p2/Controllers/BlogController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace p2.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var db = new BlogDB();
+
+            db.Database.CreateIfNotExists();
+
+            var lst = db.BlogArticles.OrderByDescending(o => o.ID).ToList();
+            ViewBag.BlogArticles = lst;
+
+            return View();
+        }
+        public ActionResult AddArticle()
+        {
+            return View();
+        }
+
+        public ActionResult ArticleSave(string subject, string body)
+        {
+            var article = new BlogArticle();
+            article.Subject = subject;
+            article.Body = body;
+            article.CreTime = DateTime.Now;
+
+            var db = new BlogDB();
+            db.BlogArticles.Add(article);
+            db.SaveChanges();
+
+            return Redirect("Index");
+        }
+
+        public ActionResult Show(int id)
+        {
+            var db = new BlogDB();
+            var article = db.BlogArticles.First(o => o.ID == id);
+
+            ViewData.Model = article;
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var db = new BlogDB();
+            var article = db.BlogArticles.First(o => o.ID == id);
+
+            ViewData.Model = article;
+            return View();
+        }
+
+        public ActionResult EditSave(int id, string subject, string body)
+        {
+            var db = new BlogDB();
+            var article = db.BlogArticles.First(o => o.ID == id);
+
+            article.Subject = subject;
+            article.Body = body;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// 删除博文
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int id)
+        {
+            var db = new BlogDB();
+            var article = db.BlogArticles.First(o => o.ID == id);
+
+            db.BlogArticles.Remove(article);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
